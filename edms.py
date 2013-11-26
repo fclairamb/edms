9707a4af820f176c7f6431a2b9fc1c1bf754f9d7
Florent Clairambault <florent@clairambault.fr>
2013-11-26 23:56:30 +0100

    Draft of device properties handling + python3 compatiblity

--- edms.py
+++ edms.py
@@ -6,7 +6,7 @@ import tornado.web
 import os
 import gettext
 import sqlalchemy
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Index
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 _ = gettext.gettext
@@ -29,6 +29,7 @@ class Config(Base):
     def __repr__(self):
         return "Config<name={name},value={value}>".format(id=self.id, name=self.name)
 
+
 class Device(Base):
     __tablename__ = "devices"
     id = Column(Integer, primary_key=True)
@@ -41,15 +42,38 @@ class Device(Base):
     def __repr__(self):
         return "Device<ident={ident},type={type}>".format(id=self.id, name=self.name)
 
+
 class DeviceLog(Base):
-    __tablename__ = 'device_logs'
+    __tablename__ = "device_logs"
     id = Column(Integer, primary_key=True)
     device_id = Column(Integer, ForeignKey('devices.id'))
     date = Column(DateTime)
     type = Column(String)
     content = Column(String)
 
+    def __repr__(self):
+        return "DeviceLog<id={id},device_id={device_id},date={date},type={type},content={content}>".format(
+            id=self.id,
+            device_id=self.device_id,
+            date=self.date,
+            type=self.type,
+            content=self.content
+        )
+
+
+class DeviceProperty(Base):
+    __tablename__ = "device_properties"
+    device_id = Column(Integer, primary_key=True)
+    name = Column(String, primary_key=True)
+    value = Column(String)
+
+# We want to be able to search a device by one of its property
+# like "ether_macaddress" = "010203040506"
+Index("device_properties_name_value", DeviceProperty.name, DeviceProperty.value)
+
+
 Base.metadata.create_all(engine)
+
 
 def conf_get(name):
         conf = session.query(Config).filter(Config.name == name).first()
@@ -57,6 +81,7 @@ def conf_get(name):
             return conf.value
         else:
             return None
+
 
 def conf_set(name, value):
         conf = session.query(Config).filter(Config.name == name).first()
@@ -72,6 +97,17 @@ if not nbLaunches:
 else:
     nbLaunches = str( int(nbLaunches) + 1 )
 conf_set("nb_launches", nbLaunches)
+
+
+def get_or_create_device(ident):
+    device = session.query(Device).filter(Device.ident == ident).first()
+    if not device:
+        device = Device(
+            ident=ident,
+            date_updated=datetime.utcnow(),
+            date_created=datetime.utcnow()
+        )
+    session.add(device)
 
 class DeviceById(tornado.web.RequestHandler):
     def get(self, id):
@@ -101,18 +137,25 @@ class About(tornado.web.RequestHandler):
 class DeviceRegister(tornado.web.RequestHandler):
     def post(self):
         data = tornado.escape.json_decode(self.request.body)
-        device = session.query(Device).filter(Device.ident == data['ident']).first()
-        if not device:
-            device = Device(
-                ident=data['ident'],
-                type=data.get('type'),
-                date_updated=datetime.utcnow(),
-                date_created=datetime.utcnow()
-            )
-            session.add(device)
+        device = get_or_create_device(data['ident'])
+        if not device.type and data.get('ident'):
+            device.type = data['ident']
+            device.date_updated = datetime.utcnow()
         device.date_seen = datetime.utcnow()
         session.commit()
         self.write({"status": "ok", "device_id": device.id})
+
+
+class DeviceProperties(tornado.web.RequestHandler):
+    def post(self):
+        data = tornado.escape.json_decode(self.request.body)
+        device = get_or_create_device(data['ident'])
+        del data['ident']
+        if device:
+            for name, value in data:
+                session.add(DeviceProperty(name, value))
+
+        session.commit()
 
 
 class DeviceEvent(tornado.web.RequestHandler):
@@ -156,6 +199,7 @@ application = tornado.web.Application(
        (r"/device/([0-9]+)", DeviceById),
        (r"/device/event", DeviceEvent),
        (r"/devices", Devices),
+       (r"/device/properties", DeviceProperties),
        (r"/", Index),
        (r"/device/register", DeviceRegister),
        (r"/about", About)
@@ -168,6 +212,6 @@ application = tornado.web.Application(
  
 if __name__ == "__main__":
     application.listen(8888)
-    print "Ready !"
+    print ("Ready !")
     tornado.ioloop.IOLoop.instance().start()
 
