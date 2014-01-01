656a2804c162c18ef66e282e50fcb6587e8069eb
Florent Clairambault <florent@clairambault.fr>
2014-01-01 19:23:31 +0100

    Device reporting python client fix: Time is in UTC

--- requests/device_report.py
+++ requests/device_report.py
@@ -4,7 +4,7 @@ import json, urllib2, datetime, os, socket
 report = {
     "hostname": socket.gethostname(),
     "type": "hello_from_python",
-    "date": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'),
+    "date": datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f'),
     "loadavg": os.getloadavg(),
     "device_group": "g1"
 }
